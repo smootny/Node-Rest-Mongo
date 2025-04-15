833e733cfe3bbb2a5a54c122feef63eaeb791397
smootny <blazejewski@post.pl>
2025-04-15 21:31:15 +0200

    added mysql and sequelize

---
+++ Nodejs-server/util/database.js
@@ -0,0 +1,8 @@
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize('node-store', 'root', 'CezokOne187', {
+    dialect: 'mysql',
+    host: 'localhost'
+})
+
+module.exports = sequelize;
